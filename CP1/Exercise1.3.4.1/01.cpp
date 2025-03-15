4ee827c486a0d3c1ec84018939a2be6baf128ebb
riteshtyagi817 <riteshtyagi725@gmail.com>
2025-03-15 20:58:14 +0530

    added more work

---
+++ CP1/Exercise1.3.4.1/01.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+	double in;
+	cin >> in;
+	printf("%7.3f",in);
+
+
+	return 0;
+
+}
---
+++ CP1/transpose.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# you are givena a 2D matrix, your job is to transpose this matrix and display the result
+A = [(1,2,3), (4,5,6)]
+print(A)
+print([*zip(*A)])

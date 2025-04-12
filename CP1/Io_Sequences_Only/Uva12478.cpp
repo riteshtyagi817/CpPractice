c9ec33a45f9117b14d7fd1c8e3cf2a6edd36bce3
riteshtyagi817 <riteshtyagi725@gmail.com>
2025-04-12 14:26:00 +0530

    added the file

---
+++ CP1/Io_Sequences_Only/Uva12478.cpp
@@ -0,0 +1,13 @@
+/* Date: Sat Apr 12 14:02:52 IST 2025
+User: ritesht(Ritesh Tyagi) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout << "KABIR" << endl;
+	return 0;
+}
+

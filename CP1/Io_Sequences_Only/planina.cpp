1402512f7365fd907c9516bfde1972e5f1f1c9e5
riteshtyagi817 <riteshtyagi725@gmail.com>
2025-04-21 23:31:32 +0530

    files added

---
+++ CP1/Io_Sequences_Only/planina.cpp
@@ -0,0 +1,27 @@
+/* Date: Mon Apr 21 22:56:28 IST 2025
+User: ritesht(Ritesh Tyagi) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int in;
+	char dummy;
+	while(1){
+
+	 	while(scanf("%d%c",&in, &dummy) != EOF){
+			
+			if(dummy == '\n') break;
+
+		}
+
+		if(feof(stdin)) break;
+		int val = pow(2,in) + 1;
+
+		printf("%.0f\n",pow(val,2));
+	}
+	return 0;
+}
+
---
+++ CP1/Io_Sequences_Only/planina.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+val = int(input(""))
+result = ((2**val + 1) ** 2)
+print(result)
+

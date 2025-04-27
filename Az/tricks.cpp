ba26a2a3e757ea5a17bb47ec658773651cae1c30
riteshtyagi817 <riteshtyagi725@gmail.com>
2025-04-27 23:11:39 +0530

    added the files

---
+++ Az/tricks.cpp
@@ -0,0 +1,15 @@
+/* Date: Sun Apr 27 08:38:39 IST 2025
+User: ritesht(Ritesh Tyagi) */
+#include<bits/stdc++.h>
+using namespace std;
+using lli  = long long;
+typedef long long lli;
+#define int long long // but you need to use signed instead of int in main return value
+#define endl '\n'
+signed main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	return 0;
+}
+

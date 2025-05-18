9ef396a4959caa1e9c45d4399fcb55769997dcd4
riteshtyagi817 <riteshtyagi725@gmail.com>
2025-05-18 08:43:16 +0530

    added more work

---
+++ CP1/Repetion_Only/Uva01124.cpp
@@ -0,0 +1,21 @@
+/* Date: Sun May 18 07:34:14 IST 2025
+User: ritesht(Ritesh Tyagi) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void solve(){
+
+	string in;
+	while(getline(cin, in))
+		cout << in << endl;
+	
+	return;
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+	return 0;
+}
+
---
+++ CP1/Repetion_Only/timeloop.cpp
@@ -0,0 +1,26 @@
+/* Date: Sun May 18 07:23:26 IST 2025
+User: ritesht(Ritesh Tyagi) */
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void solve(){
+
+	int n;
+	cin >> n;
+	for(int i = 0; i  < n;i++){
+
+		cout << i+1 << " " << "Abracadabra" << endl;
+	}
+
+	return ;
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();	
+
+
+	return 0;
+}
+
